3e76239a93dc43a98c5529cd29c96ff20d49e69d
elaine <2250021252@qq.com>
2026-09-11 12:06:54 +0800

    添加数据

---
+++ score-stat/count.js
@@ -0,0 +1,9 @@
+const scores = [
+  {name:'用户1',count: 1, score: 92/60 ,grade: '失败'},
+  {name:'用户1',count: 2, score: 45/45 ,grade: '成功'},
+  {name:'用户1',count: 3, score: 99/20 ,grade: '大失败'},
+  {name:'用户1',count: 4, score: 20/50 ,grade: '困难成功'},
+  {name:'用户2',count: 5, score: 1/30 ,grade: '大成功'},
+  {name:'用户2',count: 6, score: 12/75 ,grade: '极难成功'},
+  {name:'用户2',count: 7, score: -2/1001 ,grade: '成功'}//错误数据
+];
